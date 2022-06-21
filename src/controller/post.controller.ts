5c70b3c2e22d6a7c528ff64f22b05595aa5ba18c
doruletzz <doros.doru@yahoo.com>
2022-06-21 21:25:17 +0300

    added tags

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,41 +1,84 @@
-import { Request, Response } from 'express';
-import Router from 'express-promise-router';
-import { QueryOrder} from '@mikro-orm/core';
-import { DI } from '../index';
+import { Request, Response } from "express";
+import Router from "express-promise-router";
+import { QueryOrder } from "@mikro-orm/core";
+import { DI } from "../index";
+import { Tag } from "../domain";
 // import { Post } from '../domain';
 
 const router = Router();
 
-router.get('/:slug', async (req: Request, res: Response) => {
+router.get("/:slug", async (req: Request, res: Response) => {
   const posts = await DI.postRepository.findOne({
-    slug: req.params.slug
+    slug: req.params.slug,
   });
   res.json(posts);
 });
 
-router.get('/', async (_req: Request, res: Response) => {
+router.get(
+  "/",
+  async (
+    req: Request<
+      {},
+      {},
+      {},
+      { tags: Array<Tag>; page: number; pageSize: number }
+    >,
+    res: Response
+  ) => {
+    console.log(req.query.tags);
+    const page = req.query.page ? req.query.page : 1;
+    const pageSize = req.query.pageSize ? req.query.pageSize : 10;
+
+    console.log(req.query, page, pageSize);
+
+    const posts = await DI.postRepository.find(
+      { tags: req.query.tags },
+      {
+        orderBy: { createdAt: QueryOrder.DESC },
+        limit: pageSize,
+        offset: (page - 1) * pageSize,
+      }
+    );
+
+    res.json(posts);
+  }
+);
+
+router.get(
+  "/",
+  async (
+    req: Request<{}, {}, {}, { page: number; pageSize: number }>,
+    res: Response
+  ) => {
+    const page = req.query.page ? req.query.page : 1;
+    const pageSize = req.query.pageSize ? req.query.pageSize : 10;
+
+    console.log(req.query, page, pageSize);
+
     const posts = await DI.postRepository.findAll({
-      orderBy: { title: QueryOrder.DESC },
-      limit: 20,
+      orderBy: { createdAt: QueryOrder.DESC },
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
     });
     res.json(posts);
+  }
+);
+
+router.post("/", async (req: Request, res: Response) => {
+  console.log(req.body.slug, req.body.title);
+  if (!req.body.slug || !req.body.title) {
+    res.status(400);
+    return res.json({ message: "One of `slug or title` is missing" });
+  }
+
+  try {
+    const post = DI.postRepository.create(req.body);
+    await DI.postRepository.persist(post).flush();
+
+    res.json(post);
+  } catch (e: any) {
+    return res.status(400).json({ message: e.message });
+  }
 });
 
-router.post('/', async (req: Request, res: Response) => {
-    if (!req.body.id || !req.body.slug || !req.body.title) {
-      res.status(400);
-      return res.json({ message: 'One of `id, slug or title` is missing' });
-    }
-  
-    try {
-      const post = DI.postRepository.create(req.body);
-      await DI.postRepository.persist(post).flush();
-  
-      res.json(post);
-    } catch (e: any) {
-      return res.status(400).json({ message: e.message });
-    }
-});
-
-  
 export const PostController = router;
--- src/domain/Post.ts
+++ src/domain/Post.ts
@@ -1,32 +1,48 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import {
+  ArrayType,
+  Entity,
+  EnumArrayType,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/core";
 
 @Entity()
 export class Post {
+  @PrimaryKey({ type: "number" })
+  id!: number;
 
-    @PrimaryKey({type: 'number'})
-    id!: number;
+  @Property({ type: "date" })
+  createdAt? = new Date();
 
-    @Property({type: 'date'})
-    createdAt? = new Date();
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt? = new Date();
 
-    @Property({ type: 'date', onUpdate: () => new Date() })
-    updatedAt? = new Date();
+  @Property({ type: "string", unique: true })
+  slug!: string;
 
-    @Property({type: 'string', unique: true})
-    slug!: string;
+  @Property({ type: "string" })
+  title!: string;
 
-    @Property({type: 'string'})
-    title!: string;
-    
-    @Property({type: 'string'})
-    summary?: string;
+  @Property({ type: "string" })
+  summary?: string;
 
-    @Property({type: 'string'})
-    user?: string;
+  @Property({ type: EnumArrayType })
+  tags?: Array<Tag>;
 
-    @Property({type: 'text'})
-    content!: string;
+  @Property({ type: "string" })
+  user?: string;
 
-    @Property({type: 'string'})
-    imageUrl?: string;
+  @Property({ type: "text" })
+  content!: string;
+
+  @Property({ type: "string" })
+  imageUrl?: string;
+}
+
+export enum Tag {
+  programming,
+  blog,
+  design,
+  react,
+  books,
 }
